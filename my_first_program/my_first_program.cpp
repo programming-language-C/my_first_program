0b5746043781a5cc8657021c8916e58fc2929ce0
Roman <maks3564195@gmail.com>
2022-10-03 11:56:08 +0300

    Добавьте файлы проекта.

---
+++ my_first_program/my_first_program.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	// Объявить переменные
+	int a, b, c;
+	// ввести a и b
+	printf("Ввести a:\n");
+	scanf_s("%d", &a);
+	printf("Ввести b:\n");
+	scanf_s("%d", &b);
+	// напечатать c
+	c = a + b;
+	printf("Результат: %d", c);
+	_getch();
+}
